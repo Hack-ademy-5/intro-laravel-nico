9d358990e9a579b1bbbb677c432f5f64c8c8b147
Nicola Milella <nicombk@gmail.com>
2021-08-05 10:46:29 +0200

    stack push

--- resources/views/contact.blade.php
+++ resources/views/contact.blade.php
@@ -1,5 +1,12 @@
 @extends('base')
 @section('title','Contact Us')
+@push('css')
+<style>
+    h1 {
+      color:green
+    }
+</style>
+@endpush
 @section('content')
 <div class="row">
     <div class="col-12">
